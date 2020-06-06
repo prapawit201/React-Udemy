cf734da246b94e9e28ebeedcd0827a75941a3b8b
prapawit201 <piyapat43@gmail.com>
2020-06-07 01:08:24 +0700

    [Add] Learn React Children props

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <h1>Hi! I'm a react app</h1>
-      <Person name="Babe" age="21" birth="07-04-1999" />
+      <Person name="Babe" age="21" birth="07-04-1999">
+        I'm SIT KMUTT
+      </Person>
     </div>
   );
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,10 +2,13 @@ import React from "react";
 
 const person = (props) => {
   return (
-    <p>
-      I'm {props.name} and i'm {props.age} years old and my birthday is{" "}
-      {props.birth}{" "}
-    </p>
+    <div>
+      <p>
+        I'm {props.name} and i'm {props.age} years old and my birthday is{" "}
+        {props.birth}
+      </p>
+      <p>{props.children}</p>
+    </div>
   );
 };
 
